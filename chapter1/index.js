a239180887c26cc702b074ad84ea3ab8eb7eb9ef
fengqiong <fengqiongb@moguyun.com>
2021-07-05 15:09:06 +0800

    拆分statement 为多个函数

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,23 +1,12 @@
 const { plays } = require("./play")
 const { invoice } = require("./invoice")
 
-// const invoice = invoice[0]
-// const plays = plays
-
-function format (cur) {
-  return new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD",minimumFractionDigits: 2 }).format(cur);
+function format (num) {
+  return new Intl.NumberFormat("en-US",{ style: "currency", currency: "USD",minimumFractionDigits: 2 }).format(num / 100);
 }
-function statement () {
-  console.log(invoice, plays)
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice[0].customer}\n`;
-
-  for (let perf of invoice[0].performance) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
+function amountFor (perf) {
+  let thisAmount = 0
+  switch (playForPerf(perf).type) {
     case "tragedy":
       thisAmount = 40000;
       if (perf.audience > 30) {
@@ -32,20 +21,44 @@ function statement () {
       thisAmount += 300 * perf.audience;
       break;
     default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playForPerf(perf).type}`);
     }
+    return thisAmount
+}
 
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+function volumeCreditsFor (perf) {
+  let result = Math.max(perf.audience - 30, 0);
+  if ("comedy" === playForPerf(perf).type) result += Math.floor(perf.audience / 5);
+  return result
+}
 
-    // print line for this order
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+function playForPerf (perf) {
+  return plays[perf.playID]
+}
+
+function totalVolumeCredits() {
+  let total = 0
+  for (let perf of invoice[0].performance) {
+    total += volumeCreditsFor(perf)
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  return total
+}
+
+function totalAmount() {
+  let total = 0
+  for (let perf of invoice[0].performance) {
+    total += amountFor(perf)
+  }
+  return total
+}
+
+function statement () {
+  let result = `Statement for ${invoice[0].customer}\n`;
+  for (let perf of invoice[0].performance) {
+    result += ` ${playForPerf(perf).name}: ${format(amountFor(perf))} (${perf.audience} seats)\n`
+  }
+  result += `Amount owed is ${format(totalAmount())}\n`;
+  result += `You earned ${format(totalVolumeCredits())} credits\n`;
   return result;
 }
 
