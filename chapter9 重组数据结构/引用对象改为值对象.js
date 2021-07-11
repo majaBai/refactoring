45b1b7444192e42aa28206817076684b3d01b5f1
majaBai <452712285@qq.com>
2021-07-11 15:54:00 +0800

    查询取代派生/拆分变量/值对象和引用对象的应用场景

--- chapter9 重组数据结构/引用对象改为值对象.js
+++ chapter9 重组数据结构/引用对象改为值对象.js
@@ -5,8 +5,84 @@
 
 一般说来，不可变的数据结构处理起来更容易；但是如果要在多个模块间共享数据的变动，则应该使用引用对象
 
-
-
-
-
 */
+class Person {
+    constructor(){
+      this._telephonNumber = new TelephonNumber()
+    }
+
+    get officeAreaCode()    {return this._telephoneNumber.areaCode;}
+    set officeAreaCode(arg) {this._telephoneNumber.areaCode = arg;}
+
+    get officeNumber () {
+        return this._telephonNumber.number
+    }
+    set officeNumber (val) {
+       this._telephonNumber.number = val
+    }
+
+  }
+  
+  class TelephonNumber {
+    get areaCode()    {return this._areaCode;}
+    set areaCode(arg) {this._areaCode = arg;}
+
+    get number () {
+        return this.number
+    }
+
+    set number (val) {
+        this.number = val
+    }
+  
+  }
+
+  // 将 Person 变为值对象， 首先将内部类变成值对象
+  class TelephonNumber2 {
+      constructor(areaCode, number) {
+          this._areaCode = areaCode
+          this._number = number
+      }
+    get areaCode() {
+        return this._areaCode
+    }
+    // 移除设置函数，变成不可变数据
+    // set areaCode(arg) {
+    //     this._areaCode = arg
+    // }
+
+    get number () {
+        return this._number
+    }
+
+    // set number (val) {
+    //     this._number = val
+    // }
+
+    // 值对象可以基于值判断其相等性
+    equal (another) {
+        if (!(another instanceof TelephonNumber2)) return false
+        return this.areaCode === another.areaCode && this.number === another.number
+    }
+  }
+
+  class Person2 {
+    constructor(p){
+        this._telephonNumber = new TelephonNumber(p.officeAreaCode, p.officeNumber)
+      }
+    get officeAreaCode() {
+        return this._telephoneNumber.areaCode
+    }
+    set officeAreaCode(val) {
+        // this._telephoneNumber.areaCode = arg
+        this._telephonNumber = new TelephonNumber2 (val, this.officeNumber)
+    }
+
+    get officeNumber () {
+        return this._telephonNumber.number
+    }
+    set officeNumber (val) {
+    //    this._telephonNumber.number = val
+          this._telephonNumber = new TelephonNumber2(this.officeAreaCode, val)
+    }
+  }
